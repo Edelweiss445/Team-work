46b2f74eff9a13df64d9fc06714007f530f8fdb0
Rafael Gadimov <cowboycore@yandex.ru>
2023-02-23 11:05:08 +0300

    merge into main

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static String[] products = { "Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка" };
-    public static int[] prices = { 50, 135, 65, 53 };
+    public static String[] products = {"Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка"};
+    public static int[] prices = {50, 135, 65, 53};
     public static int MIN_COST_FOR_BONUS = 1000;
 
     // В стоимости этих товаров каждые три товара должны стоить как два:
-    public static String[] productsOnSale = { "Хлеб", "Мороженка" };
+    public static String[] productsOnSale = {"Хлеб", "Мороженка"};
 
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в магазин!");
